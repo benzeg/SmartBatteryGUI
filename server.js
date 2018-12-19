f0347f01530bc54c5b90bc0e5dc35f5c88f32d0e
Benze Gong <benzebgong@gmail.com>
2018-12-19 14:28:56 -0800

    ensure service shuts down properly

--- server.js
+++ server.js
@@ -1,34 +1,14 @@
 const express = require('express');
-const app = express();
 const port = process.env.PORT || 3000;
-const env = process.env.NODE_ENV || 'dev';
-const path = require('path');
 const bodyParser = require('body-parser');
 const api = require('./server/api.js');
- 
+const app = express();
+
 app.use(express.static(__dirname + '/www'));
 app.use(bodyParser.json());
 
-if (env === 'dev') {
-  const webpackDevMiddleware = require('webpack-dev-middleware');
-  const webpack = require('webpack');
-  const webpackConfig = require('./webpack.config.js');
-  const compiler = webpack(webpackConfig);
-
-  app.use(webpackDevMiddleware(compiler, {
-    hot: true,
-    filename: 'bundle.js',
-    publicPath: '/',
-    stats: {
-        colors: true,
-      },
-    historyApiFallback: true,
-  }));
-}
-
 app.post('/upload', api.sendAnalysis);
 app.get('/analysis', api.getAnalysis);
-
  
 const server = app.listen(port, function() {
   const host = server.address().address;
